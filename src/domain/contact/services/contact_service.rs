e0d56ca32e27cb94f12571509cf494a85a7d4b8f
Peter Knaus <peter@knaus.io>
2025-02-04 00:04:19 +0100

    Contact - Return newly created record (Contact Struct)

--- src/domain/contact/services/contact_service.rs
+++ src/domain/contact/services/contact_service.rs
@@ -3,12 +3,11 @@ use crate::domain::contact::schema::contacts::dsl::contacts;
 use crate::domain::contact::models::contact::{Contact, NewContact};
 use diesel::prelude::*;
 
-pub fn insert_contact(conn: &mut PgConnection, data: &Json<NewContact>) -> usize {
+pub fn insert_contact(conn: &mut PgConnection, data: &Json<NewContact>) -> Contact {
     use crate::domain::contact::schema::contacts::dsl::*;
-
 
     diesel::insert_into(contacts)
         .values(&data.0)
-        .execute(conn)
+        .get_result(conn)
         .expect("Error creating contact")
 }
